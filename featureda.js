ad47accaf3de13e9b7f4f6ef740f3972322035ca
kevin1234h8 <kevinang445566@gmail.com>
2023-11-14 10:14:25 +0700

    deploy

--- featureda.js
+++ featureda.js
@@ -1,1 +1,8 @@
 console.log("featured a");
+const originalString = "Hello, World! This is an example string.";
+
+const encodedString = encodeURIComponent(originalString);
+const decodedString = decodeURIComponent(encodedString);
+console.log("Original String:", originalString);
+console.log("Encoded String:", encodedString);
+console.log("Decoded String:", decodedString);
